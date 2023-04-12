ca4553c5233937a9db024946e10672ecac1af742
Tenzang <gendun.loden@gmail.com>
2023-04-12 23:29:10 +1000

    feat: color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
-	theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'dark-primary': '#16151A',
+                'dark-secondary': 'slate-800',
+                'dark-highlight': '#FCFCFA',
+                'light-primary': 'white',
+                'light-secondary': '#94A3B8',
+                'light-highlight': '#16151A'
+            }
+        }
+    },
+    plugins: []
+};
