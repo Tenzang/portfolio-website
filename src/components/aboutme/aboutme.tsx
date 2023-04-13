d2a9ac71417c900c35446232d92f77579de96bee
Tenzang <gendun.loden@gmail.com>
2023-04-13 15:43:03 +1000

    feat: convert AboutMe MUI -> Tailwind

--- src/components/aboutme/aboutme.tsx
+++ src/components/aboutme/aboutme.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Container, Typography } from '@material-ui/core';
 import { FakeTyper } from '../index';
 import { FC } from '../../util';
 import { Author } from '../../hooks/useSiteMetadata';
 
 const AboutMe: FC<Author> = ({ about, code }) => {
     return (
-        <Container maxWidth="md">
+        <>
             <FakeTyper code={code} />
-            <Typography>
-                {about.split('\n').map((paragraph, i) => (
-                    <p key={i}>{paragraph}</p>
-                ))}
-            </Typography>
-        </Container>
+            {about.split('\n').map((paragraph, i) => (
+                <p key={i} className="py-2">
+                    {paragraph}
+                </p>
+            ))}
+        </>
     );
 };
 
