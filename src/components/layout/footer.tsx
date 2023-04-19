9a44231e5033385d5a75849b66d950ec611d882e
Tenzang <gendun.loden@gmail.com>
2023-04-19 15:06:37 +1000

    feat: convert Footer MUI -> Tailwind

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,38 +1,26 @@
 import React from 'react';
-import { Typography, Link, Box } from '@material-ui/core';
 
 import { FC } from '../../util';
+import { Link } from 'gatsby';
 
 const Footer: FC = () => {
     const RADIUS = '30px';
     return (
-        <Box
-            style={{
-                borderTopLeftRadius: RADIUS,
-                borderTopRightRadius: RADIUS
-            }}
-            bgcolor="background.paper"
-            component="footer"
-            p={1}
-        >
-            <Typography align="center" variant="h5">
-                © {new Date().getFullYear()}, Built with
-                {` `}
-                <Link href="https://www.gatsbyjs.org" color="inherit">
-                    Gatsby
-                </Link>
-                ,{` `}
-                <Link href="https://www.typescriptlang.org" color="inherit">
-                    Typescript
-                </Link>{' '}
-                and
-                {` `}
-                <Link href="https://material-ui.com/" color="inherit">
-                    @Material-ui
-                </Link>
-                ,
-            </Typography>
-        </Box>
+        <footer className="p-1 rounded-t-3xl text-center bg-light-secondary dark:bg-dark-secondary">
+            Built with{' '}
+            <Link to="https://www.gatsbyjs.org" className="underline">
+                Gatsby
+            </Link>{' '}
+            ,{' '}
+            <Link to="https://www.typescriptlang.org" className="underline">
+                Typescript
+            </Link>{' '}
+            &{' '}
+            <Link to="https://www.tailwindcss.com" className="underline">
+                TailwindCSS
+            </Link>{' '}
+            <span className="text-sm">({new Intl.DateTimeFormat('en-US').format(new Date())})</span>
+        </footer>
     );
 };
 
