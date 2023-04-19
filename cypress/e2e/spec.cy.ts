73f24af17179feaa7fed813e23544b9021b61c59
Tenzang <gendun.loden@gmail.com>
2023-04-19 13:10:16 +1000

    test: NavBar

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,13 +1,43 @@
 const baseURL = 'http://127.0.0.1:8000/';
 
 describe('My First Test', () => {
-    it('Finds Main Title', () => {
+    beforeEach(() => {
         cy.visit(baseURL);
+    });
 
+    it('Finds Main Title', () => {
         cy.contains(/loden gendun/i);
+    });
 
-        cy.contains(/about/i).click();
+    describe('Nav Bar', () => {
+        let nav;
 
-        cy.url().should('include', '#about');
+        beforeEach(() => {
+            nav = cy.get('nav');
+        });
+
+        it('can navigate user to the About section', () => {
+            nav.contains(/about/i).click();
+            cy.url().should('include', '#about');
+        });
+
+        it('can navigate user to the Skills section', () => {
+            nav.contains(/skills/i).click();
+            cy.url().should('include', '#skills');
+        });
+
+        it('can navigate user to the Projects section', () => {
+            nav.contains(/projects/i).click();
+            cy.url().should('include', '#projects');
+        });
+
+        it('can navigate user to the contact section', () => {
+            nav.contains(/contact/i).click();
+            cy.url().should('include', '#contact');
+        });
+
+        it('can open resume page', () => {
+            nav.contains(/resume/i);
+        });
     });
 });
